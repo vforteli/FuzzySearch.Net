ec5c0edfc6afbe1133b01286de1aceedf328f500
Unknown <verner@fortelius.net>
2022-08-16 22:31:43 +0300

    fuzzysearch 0 distance

---
+++ FuzzySearchNet.Tests/FuzzySearchTests.cs
@@ -0,0 +1,60 @@
+using System.Text;
+
+namespace FuzzySearchNet.Tests
+{
+    public class FuzzySearchTests
+    {
+        [Test]
+        public void TestZeroMaxDistanceMultiple()
+        {
+            var word = "foo";
+            var text = "foo-----fo--foo-f--fooo--";
+            var stream = new MemoryStream(Encoding.UTF8.GetBytes(text));
+
+            var results = FuzzySearch.Find(word, stream, 0).ToList();
+
+            Assert.That(results.Count, Is.EqualTo(3));
+
+            Assert.That(results[0].StartIndex, Is.EqualTo(0));
+            Assert.That(results[0].EndIndex, Is.EqualTo(2));
+            Assert.That(results[0].Match, Is.EqualTo(word));
+
+            Assert.That(results[1].StartIndex, Is.EqualTo(12));
+            Assert.That(results[1].EndIndex, Is.EqualTo(14));
+            Assert.That(results[1].Match, Is.EqualTo(word));
+
+            Assert.That(results[2].StartIndex, Is.EqualTo(19));
+            Assert.That(results[2].EndIndex, Is.EqualTo(21));
+            Assert.That(results[2].Match, Is.EqualTo(word));
+        }
+
+        [Test]
+        public void TestZeroMaxDistanceNoMatch()
+        {
+            var word = "foo";
+            var text = "fo------fo--fo--f--fo-o--";
+            var stream = new MemoryStream(Encoding.UTF8.GetBytes(text));
+
+            var results = FuzzySearch.Find(word, stream, 0).ToList();
+
+            Assert.That(results.Count, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void TestZeroMaxDistancePerfectMatch()
+        {
+            var word = "foo";
+            var text = "foo";
+            var stream = new MemoryStream(Encoding.UTF8.GetBytes(text));
+
+            var results = FuzzySearch.Find(word, stream, 0).ToList();
+
+            Assert.That(results.Count, Is.EqualTo(1));
+
+            Assert.That(results[0].StartIndex, Is.EqualTo(0));
+            Assert.That(results[0].EndIndex, Is.EqualTo(2));
+            Assert.That(results[0].Match, Is.EqualTo(word));
+
+        }
+    }
+}
---
+++ FuzzySearchNet/FuzzySearch.cs
@@ -0,0 +1,70 @@
+﻿namespace FuzzySearchNet;
+
+public class FuzzySearch
+{
+    /// <summary>
+    /// Find instances of term in text up to maximum distance.
+    /// </summary>
+    /// <param name="term"></param>
+    /// <param name="text"></param>
+    /// <param name="maxDistance"></param>
+    public static IEnumerable<MatchResult> Find(string term, Stream text, int maxDistance = 3)
+    {
+        if (string.IsNullOrEmpty(term))
+        {
+            throw new ArgumentException("Term cannot be null", nameof(term));
+        }
+
+        if (maxDistance != 0)
+        {
+            throw new NotImplementedException();
+        }
+        else
+        {
+            return FindZeroDistance(term, text);
+        }
+    }
+
+
+    /// <summary>
+    /// Finds term in text with max distance 0, full match that is.
+    /// </summary>
+    /// <param name="term"></param>
+    /// <param name="text"></param>
+    /// <returns></returns>
+    internal static IEnumerable<MatchResult> FindZeroDistance(string term, Stream text)
+    {
+        using var streamReader = new StreamReader(text);
+
+        var needlePosition = 0;
+        var termLength = term.Length - 1;
+        var currentIndex = 0;
+
+        while (!streamReader.EndOfStream)
+        {
+            var currentCharacter = (char)streamReader.Read();
+
+            if (currentCharacter == term[needlePosition])
+            {
+                if (needlePosition == termLength)
+                {
+                    yield return new MatchResult(currentIndex - termLength, currentIndex, term);
+                    needlePosition = 0;
+                }
+                else
+                {
+                    needlePosition++;
+                }
+            }
+            else
+            {
+                needlePosition = 0;
+            }
+
+            currentIndex++;
+        }
+    }
+}
+
+
+public record MatchResult(int StartIndex, int EndIndex, string Match);
