05c931180ad8fce6ae2abc1a3d3e4ebfe26772a6
Verner Fortelius <verner@fortelius.net>
2024-02-24 23:26:26 +0200

    memory usage improvements
    - add console app for testinng allocations
    - reduce allocations in various places
    - some refactoring and cleanup
    - bump version

--- FuzzySearchNet.Benchmark/BenchmarkFuzzySearch.cs
+++ FuzzySearchNet.Benchmark/BenchmarkFuzzySearch.cs
@@ -1,4 +1,5 @@
-﻿using BenchmarkDotNet.Attributes;
+﻿using System.Text;
+using BenchmarkDotNet.Attributes;
 
 namespace FuzzySearchNet.Benchmark;
 
@@ -8,20 +9,35 @@ public class BenchmarkFuzzySearch
     private const string term2 = "fooo--foo-----fo";
     private const string text = "foo-----fo--foo-f--fooo--foo-----fo--foo-f--fooo--foo-----fo--foo-f--fooo--foo-----fo--foo-f--fooo--foo-----fo--foo-f--fooo--foo-----fo--foo-f--fooo--foo-----fo--foo-f--fooo--foo-----fo--foo-f--fooo--foo-----fo--foo-f--fooo--";
 
-    //[Benchmark]
-    //public void SubstitutionOnlyBufferingShort() => FuzzySearch.FindSubstitutionsOnlyBuffering(term, text, 1);
+    public static readonly MemoryStream textStream = new MemoryStream(Encoding.UTF8.GetBytes(text));
 
-    //[Benchmark]
-    //public void SubstitutionOnlyBufferingLong() => FuzzySearch.FindSubstitutionsOnlyBuffering(term2, text, 1);
+    [Benchmark]
+    public void SubstitutionOnlyShort() => FuzzySearch.FindSubstitutionsOnly(term, text, 1);
 
+    [Benchmark]
+    public void SubstitutionOnlyLong() => FuzzySearch.FindSubstitutionsOnly(term2, text, 1);
 
-    //[Benchmark]
-    //public void SubstitutionOnlyBufferingLong3distance() => FuzzySearch.FindSubstitutionsOnlyBuffering(term2, text, 3);
+    [Benchmark]
+    public void SubstitutionOnlyShor_3_distance() => FuzzySearch.FindSubstitutionsOnly(term, text, 3);
+
+    [Benchmark]
+    public void SubstitutionOnlyLong_3_distance() => FuzzySearch.FindSubstitutionsOnly(term2, text, 3);
+
 
 
     [Benchmark]
     public void LevenshteinLong()
     {
         _ = FuzzySearch.FindLevenshtein(term2, text, new FuzzySearchOptions(3)).ToList();
+    }
+
+    [Benchmark]
+    public async Task LevenshteinLongAsync()
+    {
+        var stream = new MemoryStream(Encoding.UTF8.GetBytes(text));    // this is not ideal, but the effect of doing this here is basically within the stddev of the benchmark
+        await foreach (var _ in FuzzySearch.FindLevenshteinAsync(term2, stream, new FuzzySearchOptions(3)))
+        {
+
+        }
     }
 }
