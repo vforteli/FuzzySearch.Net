c531b9691aa819592d2fed2884d6a64628b768fd
Verner Fortelius <verner@fortelius.net>
2024-04-16 22:06:35 +0300

    less allocations

--- FuzzySearchNet/src/Utils.cs
+++ FuzzySearchNet/src/Utils.cs
@@ -1,6 +1,4 @@
-﻿using System.Runtime.CompilerServices;
-
-namespace FuzzySearchNet;
+﻿namespace FuzzySearchNet;
 
 public static class Utils
 {
@@ -12,31 +10,49 @@ public static class Utils
     {
         var matchesEnumerator = matches.GetEnumerator();
 
-        var group = new List<CandidateMatch>();
-
         if (matchesEnumerator.MoveNext())
         {
-            group.Add(matchesEnumerator.Current);
-
+            var bestMatch = matchesEnumerator.Current;
             var matchStartIndex = matchesEnumerator.Current.StartIndex;
 
             while (matchesEnumerator.MoveNext())
             {
+                var match = matchesEnumerator.Current;
+
                 if (matchesEnumerator.Current.StartIndex > (matchStartIndex + maxDistanece))
                 {
-                    yield return GetBestMatchFromGroup(group, text);
-                    group.Clear();
+                    yield return new MatchResult
+                    {
+                        StartIndex = bestMatch.StartIndex,
+                        EndIndex = bestMatch.TextIndex,
+                        Distance = bestMatch.Distance,
+                        Match = text[bestMatch.StartIndex..bestMatch.TextIndex],
+                        Deletions = bestMatch.Deletions,
+                        Insertions = bestMatch.Insertions,
+                        Substitutions = bestMatch.Substitutions,
+                    };
+
+                    bestMatch = matchesEnumerator.Current;
                 }
 
-                group.Add(matchesEnumerator.Current);
+                if (match.Distance < bestMatch.Distance || match.Distance == bestMatch.Distance && (match.TextIndex - match.StartIndex) > (bestMatch.TextIndex - bestMatch.StartIndex))
+                {
+                    bestMatch = match;
+                }
 
                 matchStartIndex = matchesEnumerator.Current.StartIndex;
             }
-        }
 
-        if (group.Any())
-        {
-            yield return GetBestMatchFromGroup(group, text);
+            yield return new MatchResult
+            {
+                StartIndex = bestMatch.StartIndex,
+                EndIndex = bestMatch.TextIndex,
+                Distance = bestMatch.Distance,
+                Match = text[bestMatch.StartIndex..bestMatch.TextIndex],
+                Deletions = bestMatch.Deletions,
+                Insertions = bestMatch.Insertions,
+                Substitutions = bestMatch.Substitutions,
+            };
         }
     }
 
@@ -49,82 +65,49 @@ public static class Utils
     {
         var matchesEnumerator = matches.GetAsyncEnumerator();
 
-        var group = new List<MatchResultWithValue>();
-
         if (await matchesEnumerator.MoveNextAsync())
         {
-            group.Add(matchesEnumerator.Current);
-
+            var bestMatch = matchesEnumerator.Current;
             var matchStartIndex = matchesEnumerator.Current.StartIndex;
 
             while (await matchesEnumerator.MoveNextAsync())
             {
+                var match = matchesEnumerator.Current;
+
                 if (matchesEnumerator.Current.StartIndex > (matchStartIndex + maxDistanece))
                 {
-                    yield return GetBestMatchFromGroupWithValue(group);
-                    group.Clear();
+                    yield return new MatchResult
+                    {
+                        StartIndex = bestMatch.StartIndex,
+                        EndIndex = bestMatch.EndIndex,
+                        Distance = bestMatch.Distance,
+                        Match = bestMatch.Match,
+                        Deletions = bestMatch.Deletions,
+                        Insertions = bestMatch.Insertions,
+                        Substitutions = bestMatch.Substitutions,
+                    };
+
+                    bestMatch = matchesEnumerator.Current;
                 }
 
-                group.Add(matchesEnumerator.Current);
+                if (match.Distance < bestMatch.Distance || match.Distance == bestMatch.Distance && (match.EndIndex - match.StartIndex) > (bestMatch.EndIndex - bestMatch.StartIndex))
+                {
+                    bestMatch = match;
+                }
 
                 matchStartIndex = matchesEnumerator.Current.StartIndex;
             }
-        }
 
-        if (group.Any())
-        {
-            yield return GetBestMatchFromGroupWithValue(group);
-        }
-    }
-
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    internal static MatchResult GetBestMatchFromGroup(List<CandidateMatch> group, string text)
-    {
-        var bestMatch = group.First();
-
-        foreach (var match in group.Skip(1))
-        {
-            if (match.Distance < bestMatch.Distance || match.Distance == bestMatch.Distance && (match.TextIndex - match.StartIndex) > (bestMatch.TextIndex - bestMatch.StartIndex))
+            yield return new MatchResult
             {
-                bestMatch = match;
-            }
+                StartIndex = bestMatch.StartIndex,
+                EndIndex = bestMatch.EndIndex,
+                Distance = bestMatch.Distance,
+                Match = bestMatch.Match,
+                Deletions = bestMatch.Deletions,
+                Insertions = bestMatch.Insertions,
+                Substitutions = bestMatch.Substitutions,
+            };
         }
-
-        return new MatchResult
-        {
-            StartIndex = bestMatch.StartIndex,
-            EndIndex = bestMatch.TextIndex,
-            Distance = bestMatch.Distance,
-            Match = text[bestMatch.StartIndex..bestMatch.TextIndex],
-            Deletions = bestMatch.Deletions,
-            Insertions = bestMatch.Insertions,
-            Substitutions = bestMatch.Substitutions,
-        };
-    }
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    internal static MatchResult GetBestMatchFromGroupWithValue(List<MatchResultWithValue> group)
-    {
-        var bestMatch = group.First();
-
-        foreach (var match in group.Skip(1))
-        {
-            if (match.Distance < bestMatch.Distance || match.Distance == bestMatch.Distance && (match.EndIndex - match.StartIndex) > (bestMatch.EndIndex - bestMatch.StartIndex))
-            {
-                bestMatch = match;
-            }
-        }
-
-        return new MatchResult
-        {
-            StartIndex = bestMatch.StartIndex,
-            EndIndex = bestMatch.EndIndex,
-            Distance = bestMatch.Distance,
-            Match = bestMatch.Match,
-            Deletions = bestMatch.Deletions,
-            Insertions = bestMatch.Insertions,
-            Substitutions = bestMatch.Substitutions,
-        };
     }
 }
