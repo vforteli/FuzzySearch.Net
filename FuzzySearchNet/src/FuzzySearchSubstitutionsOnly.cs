12fdfb86f02b35a5b0a1ead5fd027d92877af32c
Verner Fortelius <verner@fortelius.net>
2024-05-22 22:46:49 +0300

    add options for case insensitive matching
    this avoids having to lowercase the input text thus reducing allocations. Revert some linq shenaningans with unnecessary allocations

--- FuzzySearchNet/src/FuzzySearchSubstitutionsOnly.cs
+++ FuzzySearchNet/src/FuzzySearchSubstitutionsOnly.cs
@@ -5,20 +5,27 @@ public partial class FuzzySearch
     /// <summary>
     /// Finds term in text with only substitutions
     /// </summary>     
-    public static IEnumerable<MatchResult> FindSubstitutionsOnly(string subSequence, string text, int maxDistance)
+    public static IEnumerable<MatchResult> FindSubstitutionsOnly(string subSequence, string text, int maxDistance, bool invariantCultureIgnoreCase = false)
     {
         if (string.IsNullOrEmpty(subSequence))
         {
             yield break;
         }
 
+        subSequence = invariantCultureIgnoreCase ? subSequence.ToLowerInvariant() : subSequence;
+
         for (var currentIndex = 0; currentIndex < text.Length - (subSequence.Length - 1); currentIndex++)
         {
+            var needlePosition = currentIndex;
             var candidateDistance = 0;
 
-            foreach (var (textChar, patternChar) in text[currentIndex..(currentIndex + subSequence.Length)].Zip(subSequence, (f, s) => (f, s)))
+            for (var termIndex = 0; termIndex < subSequence.Length; termIndex++)
             {
-                if (textChar != patternChar)
+                var match = invariantCultureIgnoreCase
+                    ? char.ToLowerInvariant(text[needlePosition]) == subSequence[termIndex]
+                    : text[needlePosition] == subSequence[termIndex];
+
+                if (!match)
                 {
                     candidateDistance++;
                     if (candidateDistance > maxDistance)
@@ -26,6 +33,8 @@ public partial class FuzzySearch
                         break;
                     }
                 }
+
+                needlePosition++;
             }
 
             if (candidateDistance <= maxDistance)
@@ -49,12 +58,14 @@ public partial class FuzzySearch
     /// Finds term in text with only substitutions from stream
     /// </summary>
     /// <param name="bufferSize">Default 4096. If bufferSize is less then maxdistance, it will become a multiple of bufferSize</param>
-    public static async IAsyncEnumerable<MatchResult> FindSubstitutionsOnlyAsync(string subSequence, Stream textStream, int maxDistance, int bufferSize = 4096)
+    public static async IAsyncEnumerable<MatchResult> FindSubstitutionsOnlyAsync(string subSequence, Stream textStream, int maxDistance, int bufferSize = 4096, bool invariantCultureIgnoreCase = false)
     {
         if (string.IsNullOrEmpty(subSequence))
         {
             yield break;
         }
+
+        subSequence = invariantCultureIgnoreCase ? subSequence.ToLowerInvariant() : subSequence;
 
         var subSequenceLengthMinusOne = subSequence.Length - 1;
 
@@ -68,13 +79,24 @@ public partial class FuzzySearch
 
         do
         {
+            if (bytesRead < subSequence.Length)
+            {
+                // Cant have a match if subsequence is longer than text
+                yield break;
+            }
+
             for (var currentIndex = 0; currentIndex < bytesRead - subSequenceLengthMinusOne; currentIndex++)
             {
+                var needlePosition = currentIndex;
                 var candidateDistance = 0;
 
-                foreach (var (textChar, patternChar) in buffer[currentIndex..(currentIndex + subSequence.Length)].Zip(subSequence, (f, s) => (f, s)))
+                for (var subSequenceIndex = 0; subSequenceIndex < subSequence.Length; subSequenceIndex++)
                 {
-                    if (textChar != patternChar)
+                    var match = invariantCultureIgnoreCase
+                        ? char.ToLowerInvariant(buffer[needlePosition]) == subSequence[subSequenceIndex]
+                        : buffer[needlePosition] == subSequence[subSequenceIndex];
+
+                    if (!match)
                     {
                         candidateDistance++;
                         if (candidateDistance > maxDistance)
@@ -82,6 +104,8 @@ public partial class FuzzySearch
                             break;
                         }
                     }
+
+                    needlePosition++;
                 }
 
                 if (candidateDistance <= maxDistance)
@@ -98,6 +122,7 @@ public partial class FuzzySearch
                     };
                 }
             }
+
 
             streamIndexOffset += bytesRead - subSequenceLengthMinusOne;
 
